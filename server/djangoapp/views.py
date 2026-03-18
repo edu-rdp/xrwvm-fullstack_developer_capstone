42a2cea666e33a993fb238032034b933ed3caad9
edu-rdp <eduardopaula@alunos.utfpr.edu.br>
2026-03-18 18:02:19 -0400

    Add car model assessment file and final Django updates

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,21 @@ from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
 import logging
 import json
+from .models import CarMake, CarModel
+from django.http import JsonResponse
+from .populate import initiate
+
 # from .populate import initiate
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 
 # Get an instance of a logger
