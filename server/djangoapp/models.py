d2af050f9d687629213ccd408ba9f28bfa64fe54
edu-rdp <eduardopaula@alunos.utfpr.edu.br>
2026-03-20 10:25:28 -0400

    Sanitized app.js and fixed JS notation errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,7 +6,6 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
-    
     def __str__(self):
         return self.name
 
@@ -17,11 +16,12 @@ class CarModel(models.Model):
     type = models.CharField(max_length=10, choices=[
         ('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')
     ], default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    year = models.IntegerField(
+        default=2023,
+        validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     # Although not in the sample code, the instructions mention a Dealer ID
-    dealer_id = models.IntegerField(default=1) 
-
+    dealer_id = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.car_make.name} {self.name}"
